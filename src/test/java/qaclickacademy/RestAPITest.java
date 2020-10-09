025df495d92836bb8f02a5ac7a23deaedd5349fb
Iftihor Mamajonov <m.proud78@gmail.com>
2020-10-09 14:35:15 -0500

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+    @Test
+public void postJira(){
+    System.out.println("postJira");
+}
+    @Test
+    public void deleteTwitter(){
+        System.out.println("deleteTwitter");
+    }
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+    @Test
+    public void browserAutamation(){
+        System.out.println("browserAutamation");
+    }
+    @Test
+    public void elementsUI(){
+        System.out.println("elementsUI");
+    }
+}
